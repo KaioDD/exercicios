1be0e3574d318e582da9bd5e282234140d15dff1
KaioDD <kkaiodantas6@gmail.com>
2023-07-14 09:17:15 -0300

    Feito exercicio: Calcular area do retangulo

---
+++ Area-Retangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Area_Retangulo;
+using System.Globalization;
+
+Console.WriteLine("Informe a largura e altura do triangulo");
+
+Retangulo r = new Retangulo();
+r.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+r.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.WriteLine($"Area do retangulo: {r.CalcularArea()}");
+Console.WriteLine($"Perimeto do retangulo: {r.CalcularPerimetro()}");
+Console.WriteLine($"Diagonal do retangulo: {r.CalcularDiagonal()}");
+
+
+
---
+++ Area-Retangulo/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Retangulo
+{
+    public class Retangulo
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+
+        public double CalcularArea()
+        {
+            return Altura * Largura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+        public Double CalcularDiagonal()
+        {
+            return Math.Sqrt((Altura * Altura) + (Largura * Largura));
+        }
+    }
+}
